d535f7fd4169af4921eca66e528efd87889f382e
Chris Welch <christopher.david.welch@gmail.com>
2016-11-27 23:55:46 +1300

    First Commit

---
+++ GraphicsController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
+using System.IO;
+
+namespace TestGraphicalProject {
+
+    class GraphicsController {
+
+        Canvas _canvas;
+        Storyboard _storyboard;
+        List<Shape> _shapeList;
+        Random _random;
+
+        /// <summary>
+        /// Initialises the main Canvas, but doesn't run anything yet
+        /// </summary>
+        public GraphicsController(Canvas canvas)
+        {
+            _canvas = canvas;
+            _storyboard = new Storyboard();
+            _shapeList = new List<Shape>();
+            _random = new Random();
+        }
+
+        /// <summary>
+        /// Returns false if fails
+        /// </summary>
+        public bool LoadShapeFile(string filePath)
+        {
+            _shapeList.Clear();
+
+            try
+            {
+                var stream = File.OpenText(filePath);
+
+                while (!stream.EndOfStream)
+                {
+                    var line = stream.ReadLine();
+                    Console.WriteLine(line);
+
+                    if(line == "Gunch")
+                    {
+                        throw new Exception("ghost");
+                    }
+
+                    var shape = new Rectangle();
+                    shape.Stroke = new SolidColorBrush(Colors.Red);
+                    shape.Fill = new SolidColorBrush(Colors.White);
+                    shape.Width = _random.Next(5, 60);
+                    shape.Height = _random.Next(5, 60);
+                    
+                    _shapeList.Add(shape);
+                }
+
+                stream.Close();
+
+                return true;
+            } catch
+            {
+                return false;
+            }
+
+
+
+        }
+
+        public void DrawShapes()
+        {
+            for (int i = 0; i < _shapeList.Count; i++)
+            {
+                var shape = _shapeList[i];
+
+                Canvas.SetLeft(shape, _random.Next(0, 100)) ;
+                Canvas.SetTop(shape, _random.Next(0, 100));
+
+                _canvas.Children.Add(shape);
+            }
+
+
+            //return true;
+        }
+
+        public void ClearCanvas()
+        {
+            _canvas.Children.Clear();
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace TestGraphicalProject {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window {
+
+        Canvas _mainCanvas;
+        GraphicsController _graphicsController;         
+
+        string _filePath;
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _mainCanvas = MainCanvas;
+            _graphicsController = new GraphicsController(_mainCanvas);
+
+            if (SetTextFile("Text.txt"))
+            {
+                if (_graphicsController.LoadShapeFile(_filePath))
+                {
+                    _graphicsController.DrawShapes();
+                }
+                else
+                {
+                    ShowNotification("Error reading text file");
+                }
+
+            }
+            else
+            {
+                ShowNotification("No file at location");
+            }      
+
+        }
+
+        /// <summary>
+        /// Initialises the main Canvas
+        /// </summary>
+
+        bool SetTextFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                _filePath = filePath;
+                return true;
+            }
+            
+            return false;
+        }
+
+        void ShowNotification(string notification)
+        {
+            _graphicsController.ClearCanvas();
+
+            var textBlock = new TextBlock();
+            textBlock.Text = notification;
+            textBlock.FontSize = 30;
+            textBlock.Text = notification;
+            textBlock.VerticalAlignment = VerticalAlignment.Center;
+            textBlock.HorizontalAlignment = HorizontalAlignment.Center;
+
+            RootGrid.Children.Add(textBlock);
+        }
+    }
+}
