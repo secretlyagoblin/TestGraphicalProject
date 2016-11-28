6a245a4a85dffee7985f5c4e7662ce4b90ef3e65
Chris Welch <christopher.david.welch@gmail.com>
2016-11-28 21:26:28 +1300

    Added FileWatcher. It doesn't work.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,11 +22,9 @@ namespace TestGraphicalProject {
     public partial class MainWindow : Window {
 
         Canvas _mainCanvas;
-        GraphicsController _graphicsController;         
+        GraphicsController _graphicsController;
 
-        string _filePath;
-
-        
+        string _filePath = "Text.txt";        
 
         public MainWindow()
         {
@@ -35,23 +33,13 @@ namespace TestGraphicalProject {
             _mainCanvas = MainCanvas;
             _graphicsController = new GraphicsController(_mainCanvas);
 
-            if (SetTextFile("Text.txt"))
-            {
-                if (_graphicsController.LoadShapeFile(_filePath))
-                {
-                    _graphicsController.DrawShapes();
-                }
-                else
-                {
-                    ShowNotification("Error reading text file");
-                }
+            Run();
+        }
 
-            }
-            else
-            {
-                ShowNotification("No file at location");
-            }      
-
+        void Run()
+        {
+            //CreateFileWatcher(_filePath);
+            SetGraphicsController(_filePath);
         }
 
         /// <summary>
@@ -81,6 +69,74 @@ namespace TestGraphicalProject {
             textBlock.HorizontalAlignment = HorizontalAlignment.Center;
 
             RootGrid.Children.Add(textBlock);
+        }
+
+        void SetGraphicsController(string filePath)
+        {
+            if (SetTextFile(filePath))
+            {
+                //CreateFileWatcher(filePath);
+
+
+                if (_graphicsController.LoadShapeFile(_filePath))
+                {
+                    _graphicsController.DrawShapes();
+                }
+                else
+                {
+                    ShowNotification("Error reading text file");
+                }
+
+            }
+            else
+            {
+                ShowNotification("No file at location");
+            }
+        }
+
+        void ResetGraphicsController()
+        {
+            _graphicsController.ClearCanvas();
+            SetGraphicsController(_filePath);
+        }
+
+        // Handling text file reloading
+
+        void CreateFileWatcher(string path)
+        {
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            /* Watch for changes in LastAccess and LastWrite times, and 
+               the renaming of files or directories. */
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            // Only watch text files.
+            watcher.Filter = "*.txt";
+
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+        }
+
+        void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+
+            
+
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
         }
     }
 }
